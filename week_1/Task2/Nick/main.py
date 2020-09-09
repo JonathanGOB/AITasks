b971efedc094dc8dd2af8774fa110c3bef70e2de
NickFlok <1996_nick@live.nl>
2020-09-09 22:03:53 +0200

    Make NxN board

--- week_1/Task2/Nick/main.py
+++ week_1/Task2/Nick/main.py
@@ -2,3 +2,18 @@ import string
 import random
 
 letters = string.ascii_lowercase
+rows = []
+
+
+def make_board(row_size, column_size):
+    for x in range(column_size):
+        row = []
+        for y in range(row_size):
+            row.append(random.choice(letters))
+        rows.append(row)
+
+
+make_board(10, 10)
+for x in rows:
+    print(x)
+
