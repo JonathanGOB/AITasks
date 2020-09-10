77857488874ea6f643f06f996833d3a2726a6d96
Faimire <faimireg@hotmail.com>
2020-09-10 21:21:51 +0200

    Update main.py

--- week_1/Task2/jonathan/main.py
+++ week_1/Task2/jonathan/main.py
@@ -1,7 +1,16 @@
+import string
+import random
+
 class Board:
 
     def __init__(self, length, height):
-        self.playground = [[] for e in range(height)]
-        print(self.playground)
+        self.playground = [[random.choice(string.ascii_lowercase) for e in range(length)] for e in range(height)]
+    
+    def __str__(self):
+        display = []
+        for row in self.playground:
+            display.append(" ".join(row))
+        return '\n'.join(display)
 
-Board(5,5)
+board = Board(5,5)
+print(board)
