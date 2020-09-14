5e319025c68aa22339cb204a54fafbc824feb512
Faimire <faimireg@hotmail.com>
2020-09-14 19:13:30 +0200

    better

--- week_1/Task2/jonathan/main.py
+++ week_1/Task2/jonathan/main.py
@@ -45,7 +45,6 @@ def solve_board_with_data(board, prefixes, full_words, found, word=None, state=N
                         solve_board_with_data(new_board, prefixes, full_words, found, word + board.playground[e[0] + temp[0]][e[1] + temp[1]][0], [e[0] + temp[0], e[1] + temp[1]], start_position)
                     if word + board.playground[e[0] + temp[0]][e[1] + temp[1]][0] in full_words and word + board.playground[e[0] + temp[0]][e[1] + temp[1]][0] not in found:
                         found.append(word + board.playground[e[0] + temp[0]][e[1] + temp[1]][0])
-                        solve_board_with_data(new_board, prefixes, full_words, found)
 
     if not start_position: # search new letter
         for e in range(len(board.playground)):
@@ -56,34 +55,10 @@ def solve_board_with_data(board, prefixes, full_words, found, word=None, state=N
                         solve_board_with_data(new_board, prefixes, full_words, found, new_board.playground[e][i][0], [e, i], [e, i])
     
     return found
-
-def solve_easy(board,  prefixes, full_words, found, word=None, state=None, start_position=None):
-    if start_position: # search next from this state
-        for e in neighborOffsets:
-            temp = copy.deepcopy(state)
-            if state[0] == len(board.playground) - 1 and e[0] == 1:
-                temp[0] = -1
-            if state[1] == len(board.playground) - 1 and e[1] == 1:
-                temp[1] = -1
-            if word + board.playground[e[0] + temp[0]][e[1] + temp[1]][0] in prefixes or word + board.playground[e[0] + temp[0]][e[1] + temp[1]][0] in full_words:
-                new_board = board.add_child(copy.deepcopy(board)) 
-                if word + board.playground[e[0] + temp[0]][e[1] + temp[1]][0] in prefixes:
-                    solve_easy(new_board, prefixes, full_words, found, word + board.playground[e[0] + temp[0]][e[1] + temp[1]][0], [e[0] + temp[0], e[1] + temp[1]], start_position)
-                if word + board.playground[e[0] + temp[0]][e[1] + temp[1]][0] in full_words and word + board.playground[e[0] + temp[0]][e[1] + temp[1]][0] not in found:
-                    found.append(word + board.playground[e[0] + temp[0]][e[1] + temp[1]][0])
-
-    if not start_position: # search new letter
-        for e in range(len(board.playground)):
-            for i in range(len(board.playground[e])):
-                new_board = board.add_child(copy.deepcopy(board))
-                new_board.playground[e][i][1] = (e + 1) * (i + 1)
-                solve_easy(new_board, prefixes, full_words, found, new_board.playground[e][i][0], [e, i], [e, i])
-    
-    return found
 if __name__ == "__main__":
     board = Board([[[random.choice(string.ascii_lowercase), 0] for e in range(10)] for e in range(10)])
     print(board)
     prefixes, full_words = prepare_data(open(os.getcwd() + "\\week_1\\Task2\\jonathan\\words.txt", "r"), open(os.getcwd() + "\\week_1\\Task2\\jonathan\\words.txt", "r"))
-    answers = solve_easy(board, prefixes, full_words, [])
+    answers = solve_board_with_data(board, prefixes, full_words, [])
     for answer in answers:
         print(answer)
