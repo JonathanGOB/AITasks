d08016885c1f973e0bc853648137688626530255
Faimire <faimireg@hotmail.com>
2020-09-14 13:44:39 +0200

    Update main.py

--- week_1/task1/jonathan/main.py
+++ week_1/task1/jonathan/main.py
@@ -1,5 +1,5 @@
 class Node:
-    
+
     def __init__(self, data, parent=None):
         self.data = data
         self.parent = parent
@@ -11,8 +11,8 @@ class Node:
         child.path = self.path + [self.data]
         return child
 
-found = []
-def gwcgenerator(parent):
+
+def gwcgenerator(parent, found):
     data = parent.data.split('|')
     for e in data[0]:
         if move_right(parent.data, e) in parent.path:
@@ -29,42 +29,47 @@ def gwcgenerator(parent):
                 continue
             child1 = child.add_child(Node(move_left(child.data, i), child))
             if not check_failure(child1.data):
-                gwcgenerator(child1)
+                gwcgenerator(child1, found)
     return found
 
+
 def print_path(node):
-    if node.parent != None:
-        print_path(node.parent) 
+    if node.parent is not None:
+        print_path(node.parent)
     print("→ " + node.data, end=' ')
+
 
 def check_failure(string):
     death = ["CG", "GW", "CGW"]
     string = string.split('|')
     for e in death:
-        if e == string[0]:
-            return True
-        if e == string[1]:
+        if e == string[0] or e == string[1]:
             return True
     return False
+
 
 def move_right(whole_string, character):
     position = whole_string.find(character)
     whole_string = whole_string[:position] + whole_string[position + 1:] + character
     position = whole_string.find("F")
     whole_string = whole_string[:position] + whole_string[position + 1:] + "F"
-    whole_string = ''.join(sorted(list(whole_string.split("|")[0]))) + "|" + ''.join(sorted(list(whole_string.split("|")[1])))
+    whole_string = ''.join(sorted(list(whole_string.split("|")[0]))) + "|" + ''.join(
+        sorted(list(whole_string.split("|")[1])))
     return whole_string
+
 
 def move_left(whole_string, character):
     position = whole_string.find(character)
-    whole_string = character+ whole_string[:position] + whole_string[position + 1:]
+    whole_string = character + whole_string[:position] + whole_string[position + 1:]
     position = whole_string.find("F")
     whole_string = "F" + whole_string[:position] + whole_string[position + 1:]
-    whole_string = ''.join(sorted(list(whole_string.split("|")[0]))) + "|" + ''.join(sorted(list(whole_string.split("|")[1])))
+    whole_string = ''.join(sorted(list(whole_string.split("|")[0]))) + "|" + ''.join(
+        sorted(list(whole_string.split("|")[1])))
     return whole_string
 
+
 if __name__ == "__main__":
-    gwcgenerator(Node('CFGW|'))
+    found = gwcgenerator(Node('CFGW|'), [])
     for e in range(len(found)):
         print()
         print_path(found[e])
