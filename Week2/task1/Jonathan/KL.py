2704482fa04795ba9760144db7e6bd723f3a1e33
Faimire <faimireg@hotmail.com>
2020-09-21 14:55:53 +0200

    Update KL.py

--- Week2/task1/Jonathan/KL.py
+++ Week2/task1/Jonathan/KL.py
@@ -6,9 +6,10 @@ import numpy as np
 def distance(A, B):
     return math.hypot(A.x - B.x, A.y - B.y)
 
+
 def tour_length(tour):
     # the total of distances between each pair of consecutive cities in the tour
-    return sum(distance(tour[i], tour[i-1])
+    return sum(distance(tour[i], tour[i - 1])
                for i in range(len(tour)))
 
 
@@ -22,6 +23,7 @@ def orientation(p1, p2, p3):
     else:
         return 0
 
+
 def is_intersect(p1, p2, q1, q2):
     op1 = orientation(p1, q1, p2)
     op2 = orientation(p1, q1, q2)
@@ -32,13 +34,16 @@ def is_intersect(p1, p2, q1, q2):
         return True
     return False
 
+
 def onSegment(p, q, r):
     return distance(p, q) + distance(r, q) == distance(p, r)
 
+
 def dist(city1, city2):
-    dist = [(a - b)**2 for a, b in zip(city1, city2)]
+    dist = [(a - b) ** 2 for a, b in zip(city1, city2)]
     dist = math.sqrt(sum(dist))
     return dist
+
 
 def two_opt(cities):
     minchange = -1
@@ -46,9 +51,11 @@ def two_opt(cities):
         minchange = 0
         for i in range(0, len(cities) - 2):
             for j in range(i + 2, len(cities) - 1):
-                change =  dist(cities[i], cities[j]) + dist(cities[i+1],cities[j+1]) - dist(cities[i],cities[i+1]) - dist(cities[j],cities[j+1]);
+                change = dist(cities[i], cities[j]) + dist(cities[i + 1], cities[j + 1]) - dist(cities[i], cities[
+                    i + 1]) - dist(cities[j], cities[j + 1]);
                 if (minchange > change):
                     minchange = change;
-                    mini = i; minj = j;
+                    mini = i;
+                    minj = j;
                     cities[mini + 1:minj + 1] = cities[mini + 1:minj + 1][::-1]
     return cities
